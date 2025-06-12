12aeec64fa6c7fece5ab0dc90045571af4b3ab34
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-06-12 19:38:02 -0300

    Actualizar electrodomesticos12_6.sql

--- electrodomesticos12_6.sql
+++ electrodomesticos12_6.sql
@@ -2,20 +2,21 @@ DROP DATABASE IF EXISTE electrodomesticos;
 CREATE DATABASE electrodomesticos;
 USE electrodomesticos;
 
-CREATE TABLE personal(
-id INT PRIMARY KEY 
-
-
-
-
-
+CREATE TABLE sucursales (
+id INT PRIMARY KEY, 
+ubicacion VARCHAR(60), 
+localidad VARCHAR(60), 
+m2 INT
 ) ;
 
-CREATE TABLE sucursales (
-
-
-
-
+CREATE TABLE personal(
+id INT PRIMARY KEY AUTO_INCREMENT, 
+dni INT, 
+salario REAL, 
+f_ingreso DATETIME, 
+rol ENUM("vendedor", "cadete", "repositor", " cajero"), 
+sucursal INT, 
+FOREIGN KEY (sucursal) REFERENCES sucursales(id) 
 ) ;
 
 CREATE TABLE ventas (
