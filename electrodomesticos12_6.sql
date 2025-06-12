3eadaa4370c8b13fd66a174200db073241732fdd
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-06-12 19:43:29 -0300

    Actualizar electrodomesticos12_6.sql

--- electrodomesticos12_6.sql
+++ electrodomesticos12_6.sql
@@ -3,25 +3,25 @@ CREATE DATABASE electrodomesticos;
 USE electrodomesticos;
 
 CREATE TABLE sucursales (
-id INT PRIMARY KEY, 
-ubicacion VARCHAR(60), 
-localidad VARCHAR(60), 
-m2 INT
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    ubicacion VARCHAR(100), 
+    localidad VARCHAR(100), 
+    m2 INT
 ) ;
 
 CREATE TABLE personal(
-id INT PRIMARY KEY AUTO_INCREMENT, 
-dni INT, 
-salario REAL, 
-f_ingreso DATETIME, 
-rol ENUM("vendedor", "cadete", "repositor", " cajero"), 
-sucursal INT, 
-FOREIGN KEY (sucursal) REFERENCES sucursales(id) 
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    dni INT, 
+    salario DECIMAL(12,2), 
+    f_ingreso DATE, 
+    rol ENUM("vendedor", "cadete", "repositor", " cajero"), 
+    sucursal INT, 
+    FOREIGN KEY (sucursal) REFERENCES sucursales(id) 
 ) ;
 
 CREATE TABLE ventas (
-
-
-
-
+    id INT PRIMARY KEY AUTO_INCREMENT, 
+    vendedor INT, 
+    monto DECIMAL(20,2), 
+    FOREIGN KEY (vendedor) REFERENCES personal(id) 
 ) ;
