dacec5fae7010733fb3b4b3ec5f1e475cce5585b
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-06-12 19:24:40 -0300

    Crear electrodomesticos12_6.sql

---
+++ electrodomesticos12_6.sql
@@ -0,0 +1,26 @@
+DROP DATABASE IF EXISTE electrodomesticos;
+CREATE DATABASE electrodomesticos;
+USE electrodomesticos;
+
+CREATE TABLE personal(
+id INT PRIMARY KEY 
+
+
+
+
+
+) ;
+
+CREATE TABLE sucursales (
+
+
+
+
+) ;
+
+CREATE TABLE ventas (
+
+
+
+
+) ;
