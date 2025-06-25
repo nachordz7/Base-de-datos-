412ae43deb89e045f0aafdc1f2f1b02cd2924181
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-06-24 22:21:08 -0300

    Update concesionario24_6.sql

--- concesionario24_6.sql
+++ concesionario24_6.sql
@@ -2,18 +2,23 @@ DROP DATABASE IF EXISTS concesionario;
 CREATE DATABASE concesionario ;
 USE concesionario;
 
-CREATE TABLE personas(
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(100),
-    nombre VARCHAR(60),
-    apellido VARCHAR(60),
-    edad INT 
-);
-
 CREATE TABLE autos(
     id INT AUTO_INCREMENT PRIMARY KEY,
     patente VARCHAR(20),
     marca VARCHAR(60),
     modelo VARCHAR(60),
-    anio DATE 
+    anio YEAR,
+    duenio INT
 );
+
+CREATE TABLE personas(
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    email VARCHAR(100),
+    nombre VARCHAR(60),
+    apellido VARCHAR(60),
+    edad INT,
+    coche INT,
+    FOREIGN KEY (coche) REFERENCES autos(id)
+);
+
+ ALTER TABLE autos ADD FOREIGN KEY (duenio) REFERENCES personas (id);
