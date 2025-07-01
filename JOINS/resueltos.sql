5e41ed77436b767eb05ce1646f5c1795fd51d0fe
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-07-01 20:54:34 -0300

    Update resueltos.sql

--- JOINS/resueltos.sql
+++ JOINS/resueltos.sql
@@ -1,1 +1,23 @@
+/*1. Buscar todos los apellidos de la tabla clientes, sin repetir filas.*/
+SELECT DISTINCT apellido FROM clientes;
++-----------+
+| apellido  |
++-----------+
+| Perez     |
+| Garcia    |
+| Rodriguez |
+| Martinez  |
+| Gonzalez  |
++-----------+
 
+/*2. Dar una lista de los clientes indicando: nombre, apellido y nombre de ciudad.*/
+
+/*3. Buscar las ciudades de la provincia de Cordoba.*/
+
+/*4. Indicar fecha y hora de las ventas de biromes azules.*/
+
+/*5. ¿Cuantas biromes azules se vendieron?*/
+
+/*6. Buscar las ciudades con mas de 500000 habitantes, indicando nombre, provincia y poblacion.*/
+
+/*7. Indicar el total gastado en cada compra.*/
