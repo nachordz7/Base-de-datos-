61125f047b01d6bcf3b2541f237adcb7c20d7c36
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-08-05 22:35:48 -0300

    Update resueltos.sql

--- JOINS/resueltos.sql
+++ JOINS/resueltos.sql
@@ -1,23 +1,31 @@
 /*1. Buscar todos los apellidos de la tabla clientes, sin repetir filas.*/
 SELECT DISTINCT apellido FROM clientes;
-+-----------+
-| apellido  |
-+-----------+
-| Perez     |
-| Garcia    |
-| Rodriguez |
-| Martinez  |
-| Gonzalez  |
-+-----------+
 
 /*2. Dar una lista de los clientes indicando: nombre, apellido y nombre de ciudad.*/
-
+SELECT cl.nombre, cl.apellido, c.nombre  FROM clientes AS cl 
+  JOIN ciudades AS c ON cl.localidad = c.id;
+  
 /*3. Buscar las ciudades de la provincia de Cordoba.*/
-
+SELECT c.nombre FROM ciudades AS c
+  JOIN provincias AS p ON c.provincia = p.id
+  WHERE p.nombre = 'Cordoba';
+  
 /*4. Indicar fecha y hora de las ventas de biromes azules.*/
+SELECT fecha FROM compras AS c 
+JOIN productos AS p ON c.producto = p.id 
+WHERE descripcion = 'Birome azul';
 
 /*5. ¿Cuantas biromes azules se vendieron?*/
+SELECT SUM(c.cant) AS biromes_azules_vendidos FROM compras AS c 
+  JOIN productos AS p ON c.producto = p.id
+  WHERE p.descripcion = 'Birome azul';
 
 /*6. Buscar las ciudades con mas de 500000 habitantes, indicando nombre, provincia y poblacion.*/
 
 /*7. Indicar el total gastado en cada compra.*/
+
+/*8*/
+
+/*9. Indicar para cada compra el id de compra y la dirección de envío.*/
+
+/*10*/
