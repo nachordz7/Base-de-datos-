b975c32b3c2fde191d9fad936a68ff510762077b
nachordz7 <150174090+nachordz7@users.noreply.github.com>
2025-08-12 22:29:37 -0300

    Update resueltos.sql

--- JOINS/resueltos.sql
+++ JOINS/resueltos.sql
@@ -21,7 +21,9 @@ SELECT SUM(c.cant) AS biromes_azules_vendidos FROM compras AS c
   WHERE p.descripcion = 'Birome azul';
 
 /*6. Buscar las ciudades con mas de 500000 habitantes, indicando nombre, provincia y poblacion.*/
-
+SELECT c.nombre, p.nombre, c.poblacion FROM ciudades AS c
+  JOIN provincias AS p ON c.provincia = p.id 
+  WHERE c.poblacion > 500000 ; 
 /*7. Indicar el total gastado en cada compra.*/
 
 /*8*/
@@ -42,3 +44,40 @@ SUM(precio * cant) AS 'total gastado'
 FROM compras AS co
 JOIN clientes AS cl.id = co.cliente
 JOIN productos AS p.id = co.producto;
+
+/*12. ¿Cual es el cliente que gastó más?*/
+SELECT CONCAT(UPPER(cli.apellido), ', ', cli.nombre) AS cliente,
+       SUM(p.precio*cant) AS 'total gastado'
+FROM compras AS c
+  JOIN productos AS p ON c.producto = p.id
+  JOIN clientes AS cli ON c.cliente = cli.id
+GROUP BY cliente
+ORDER BY SUM(p.precio*cant) DESC
+LIMIT 1;
+
+/*13. Dar los nombres de los clientes que gastaron $5000 o mas.*/
+SELECT CONCAT(UPPER(cli.apellido), ', ', cli.nombre) AS cliente, 
+       SUM(p.precio*cant) AS 'total gastado' 
+FROM compras AS c 
+  JOIN productos AS p ON c.producto = p.id 
+  JOIN clientes AS cli ON c.cliente = cli.id 
+GROUP BY cliente 
+HAVING SUM(p.precio*cant) >= 5000 
+ORDER BY cli.apellido, cli.nombre;
+
+/*14. El correo necesita imprimir las etiquetas con el nombre del cliente y la dirección para cada envío. Dar una lista
+con esos datos para cada compra ordenados por fecha. En una columna apellido y nombre de cliente, en la otra
+el domicilio indicando: calle, altura, localidad y provincia.*/
+
+/*15. Indicar nombre de provincia y poblacion total de sus ciudades para la tabla ciudades. Mostrar solamente las
+provincias que sumen más de un millón de habitantes.*/
+
+/*16. Mostrar las compras de Juan Perez ordenadas por fecha. Indicar artículo, precio, cantidad y total de la compra.*/
+
+/*17. Buscar el nombre y apellido de los clientes que viven en Santa Fe o Cordoba.*/
+
+/*18. Dar el total de población para cada región de la Argentina.*/
+
+/*19. Listar las regiones del país que no lleguen a tres millones de habitantes.*/
+
+/*20. Mostrar las compras de menos de mil pesos en un listado como el del ejercicio 10*/
